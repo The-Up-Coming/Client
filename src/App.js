b4f1ee7e1fcd73ddf8c754d79a050419468035ea
Ayana Hawk <ayanahawk@Ayanas-MacBook-Pro.local>
2020-02-11 21:31:53 -0800

    added reg form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login/login'
+import Register from './components/Register/register'
 
 function App() {
   return (
     <div className="App">
     
 
-<Login/>
+<Register/>
       
     </div>
   );
--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -1,33 +1,78 @@
 import React, { Component } from "react";
-import './login.css';
+import "./register.css";
 
-class Login extends Component {
-
-  handleChange=event=>{
-    this.state={
-      :'',
-    }
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: "",
+      email: "",
+      password: "",
+      confirmPassword: ""
+    };
   }
+  handleChange = event => {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
 
-
-handleSubmit=event=>{
-  event.preventDefault();
-  this.props.onSearch(this.state.searchText);
-}
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.regUser(this.state);
+  };
 
   render() {
-    return <div>
-    
-    <h2 className="lit">Login</h2>
-    <form id="loginForm" onSubmit={this.handleSubmit}>
-      <input>
-      </input>
+    return (
+      <div>
+        
+        <div className="container">
+        <h2 className="reg">Register</h2>
+        <div className="reg-card">
+          <form className="regForm" onSubmit={this.onSubmit}>
+            <p>First Name</p>
+            <input
+              type="text"
+              name="fname"
+              id="fname"
+              value={this.state.fname}
+              onChange={this.handleChange}
+            ></input>
+<p>Email</p>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            ></input>
+<p>Password</p>
+            <input
+              type="text"
+              name="password"
+              id="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            ></input>
 
-      <input>
-      </input>
-    </form>
-    </div>;
+<p>Password></p>
+            <input
+              type="text"
+              name="confirmPassword"
+              id="confirmPassword"
+              value={this.state.confirmPassword}
+              onChange={this.handleChange}
+            ></input>
+
+            <button>Submit</button>
+            <button>Log In</button>
+          </form>
+        </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Login;
+export default Register;
